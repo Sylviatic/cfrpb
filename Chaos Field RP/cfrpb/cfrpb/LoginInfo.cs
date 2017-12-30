6ae479c0482f139b9b53d11af874125928ecf350
Sylviatic <arca9ies@gmail.com>
2017-12-30 22:00:08 +0100

    Discord.Net basecode

---
+++ Chaos Field RP/cfrpb/cfrpb/LoginInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cfrpb
+{
+    public class LoginInfo
+    {
+
+        public string token = "Mzk2NjM1MjU1NTg0MzI1NjMy.DSmPmQ.TkvnPvLhOxL0u_1Wa-QUEW5NxZc";
+      
+    }
+}
