1c6c8e991537b2837fc96c3e21492b88f1ce02ef
Sylviatic <arca9ies@gmail.com>
2017-12-30 22:20:41 +0100

    Added command: cs!bio

---
+++ Chaos Field RP/cfrpb/cfrpb/Modules/Commands.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+using Discord;
+
+namespace cfrpb.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("bio")]
+        public async Task HWC()
+        {
+            await ReplyAsync("Here is a Bio for <user>");
+        }
+
+
+
+
+
+
+    }
+}
